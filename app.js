6528c6cc18dea21b5f0c418697b0bec056a0853f
Neeraj Yadav <yneeraj9111@gmail.com>
2021-12-14 15:16:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+slider();
+function slider() {
+    const slides = document.querySelectorAll(".slide");
+    const prevBtn = document.querySelector(".prevBtn");
+    const nextBtn = document.querySelector(".nextBtn");
+
+
+
+    slides.forEach(function (slide, index) {
+        slide.style.left = `${index * 100}%`;
+    });
+
+    let counter = 0;
+
+    nextBtn.addEventListener("click", function () {
+        counter++;
+        carousel()
+    });
+
+    prevBtn.addEventListener("click", function () {
+        counter--;
+        carousel()
+    });
+
+    function carousel() {
+
+        if (counter === slides.length) {
+            counter = 0;
+        }
+        if (counter < 0) {
+            counter = slides.length - 1;
+        }
+
+        slides.forEach(function (slide) {
+            slide.style.transform = `translateX(-${counter * 100}%)`;
+        });
+
+    };
+
+    setInterval(slider, 1000)
+}
+
